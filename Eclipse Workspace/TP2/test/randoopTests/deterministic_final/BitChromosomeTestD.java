173115b6cc05b3dff72a7718595fa1302a04ddd6
Elisa Boselli <elisaboselli@hotmail.com>
2018-07-01 18:24:12 -0300

    New randoop tests

---
+++ Eclipse Workspace/TP2/test/randoopTests/deterministic_final/BitChromosomeTestD.java
@@ -0,0 +1,10 @@
+package randoopTests.deterministic_final;
+
+import org.junit.runner.RunWith;
+import org.junit.runners.Suite;
+
+@RunWith(Suite.class)
+@Suite.SuiteClasses({ BitChromosomeTestD0.class })
+public class BitChromosomeTestD {
+}
+
---
+++ Eclipse Workspace/TP2/test/randoopTests/nodeterministic_final/BitChromosomeTestND.java
@@ -0,0 +1,10 @@
+package randoopTests.nodeterministic_final;
+
+import org.junit.runner.RunWith;
+import org.junit.runners.Suite;
+
+@RunWith(Suite.class)
+@Suite.SuiteClasses({ BitChromosomeTestND0.class })
+public class BitChromosomeTestND {
+}
+
