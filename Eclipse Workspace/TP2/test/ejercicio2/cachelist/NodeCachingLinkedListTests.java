a82420bd9d3d86387418429205a80a1e8a8144ef
letcharren <leo_etcharren@hotmail.com>
2018-07-03 17:12:02 -0300

    Eliminados el resto de los mutantes ejercicio 2, arreglado try catch excepciones de Refleccion

--- Eclipse Workspace/TP2/test/ejercicio2/cachelist/NodeCachingLinkedListTests.java
+++ Eclipse Workspace/TP2/test/ejercicio2/cachelist/NodeCachingLinkedListTests.java
@@ -1,47 +1,57 @@
 package ejercicio2.cachelist;
 
 import static org.junit.Assert.*;
-
 import org.junit.experimental.theories.Theories;
 import org.junit.experimental.theories.Theory;
 import org.junit.runner.RunWith;
-
 import java.lang.reflect.*;
-
-import ejercicio2.cachelist.NodeCachingLinkedList;
 import static org.hamcrest.core.Is.*;
-import static org.hamcrest.core.IsNot.*;
 import static org.hamcrest.CoreMatchers.nullValue;
-
-
-import static org.junit.Assume.*;
 
 @RunWith(Theories.class)
 public class NodeCachingLinkedListTests {
+	
 	//Se chequea q se agrega un elemento a la lista y el modCount(no hay elementos con el mismo valor en la lista)
 	@Theory
 	public void addElement(@NCLLGenerator(min=0,max=3) NodeCachingLinkedList list) throws NoSuchFieldException, SecurityException, IllegalArgumentException, IllegalAccessException {
 		//Chequeo modCount con Reflexion
-		Field f = list.getClass().getDeclaredField("modCount");
-		f.setAccessible(true);
-		int l = (int) f.get(list);
-		list.add(new Integer(4));
-		int l2 = (int) f.get(list);
+		boolean add = false;
+		int oldMC = 0, newMC = 0;
+		try {
+			Field field = list.getClass().getDeclaredField("modCount");
+			field.setAccessible(true);
+			oldMC = (int) field.get(list);
+			add = list.add(new Integer(4));
+			newMC = (int) field.get(list);
+		}
+		catch (NoSuchFieldException | SecurityException | IllegalArgumentException | IllegalAccessException e) {
+			fail("Error usando refleccion: " + e); // Assert
+			e.printStackTrace();
+		} 
+		assertThat("verificando que el elemento es contenido en la lista", add, is(true)); // Assert
 		assertThat("verificando que el elemento es contenido en la lista", list.contains(new Integer(4)), is(true)); // Assert
-		assertThat("verificando que el elemento es contenido en la lista", l, is(l2-1)); // Assert
+		assertThat("verificando que el elemento es contenido en la lista", newMC, is(oldMC+1)); // Assert
+		assertThat("verificando que el elemento es contenido en la lista", newMC, is(oldMC+1)); // Assert
 
 	}
 	
 	//Se chequea en Add Size y Cache Size ademas de que si el elemento se agrega correctamente (podria haber otro elemento con el mismo valor)
 	@Theory
-	public void addElementCheckCacheSize(@NCLLGenerator(min=0,max=3) NodeCachingLinkedList list) throws NoSuchFieldException, SecurityException, IllegalArgumentException, IllegalAccessException {
+	public void addElementCheckCacheSize(@NCLLGenerator(min=0,max=3) NodeCachingLinkedList list) {
 		//Chequeo size con Reflexion
-		Field f = list.getClass().getDeclaredField("size");
-		f.setAccessible(true);
-		int oldSize = (int) f.get(list);
-		int oldCache = list.cacheSize;
-		list.add(new Integer(3));
-		int newSize = (int) f.get(list);
+		Field field;
+		int newSize = 0, oldSize=0, oldCache=0;
+		try {
+			field = list.getClass().getDeclaredField("size");
+			field.setAccessible(true);
+			oldSize = (int) field.get(list);
+			oldCache = list.cacheSize;
+			list.add(new Integer(3));
+			newSize = (int) field.get(list);
+		} catch (NoSuchFieldException | SecurityException | IllegalArgumentException | IllegalAccessException e) {
+			fail("Error usando refleccion: " + e); // Assert
+			e.printStackTrace();
+		}
 		assertThat("verificando que el elemento es contenido en la lista", list.contains(new Integer(3)), is(true)); // Assert
 		assertThat("verificando que se el size de la lista", newSize, is(oldSize+1)); // Assert
 		assertThat("verificando que si se utilizo la cache en caso de ser posible", list.cacheSize, is(oldCache==0? oldCache : oldCache-1)); // Assert
@@ -96,7 +106,7 @@ public class NodeCachingLinkedListTests {
 			list.remove(new Integer(4));
 			newSize = (int) size.get(list);
 		} catch (NoSuchFieldException | SecurityException | IllegalArgumentException | IllegalAccessException e) {
-			// TODO Auto-generated catch block
+			fail("Error usando refleccion: " + e); // Assert
 			e.printStackTrace();
 		}
 		assertThat("verificando que el elemento es removido de la lista", list.contains(new Integer(4)), is(false)); // Assert
@@ -109,6 +119,7 @@ public class NodeCachingLinkedListTests {
 	public void removeElementMC(@NCLLGenerator(min=0,max=3) NodeCachingLinkedList list) {
 		Field mC;
 		int cacheOld=0,oldMC=0,newMC = 0;
+		boolean removeOK=false;
 		try {
 			mC = list.getClass().getDeclaredField("modCount");
 			mC.setAccessible(true);
@@ -116,12 +127,13 @@ public class NodeCachingLinkedListTests {
 			list.add(obj);
 			oldMC = (int) mC.get(list);
 			cacheOld = list.cacheSize;
-			list.remove(new Integer(4));
+			removeOK = list.remove(new Integer(4));
 			newMC= (int) mC.get(list);
 		} catch (NoSuchFieldException | SecurityException | IllegalArgumentException | IllegalAccessException e) {
-			// TODO Auto-generated catch block
+			fail("Error usando refleccion: " + e); // Assert
 			e.printStackTrace();
 		}
+		assertThat("verificando que el elemento es removido de la lista", removeOK, is(true)); // Assert
 		assertThat("verificando que el elemento es removido de la lista", list.contains(new Integer(4)), is(false)); // Assert
 		assertThat("verificando que el elemento Eliminado fue agregado a la cache", list.cacheSize, is(cacheOld+1)); // Assert
 		assertThat("verificando que modCount es modificado", newMC, is(oldMC+1)); // Assert
